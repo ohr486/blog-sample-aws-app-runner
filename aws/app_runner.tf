d563dfcb13adf3e2b51833d13c1ead56731becf7
ohr486 <ohr486@gmail.com>
2021-06-27 05:18:46 +0900

    update aws

--- aws/app_runner.tf
+++ aws/app_runner.tf
@@ -7,7 +7,7 @@ resource "aws_apprunner_service" "blog_sample_app_runner" {
     }
     image_repository {
       image_configuration {
-        port = "3000"
+        port = "8080"
       }
       image_identifier      = "${aws_ecr_repository.blog_sample_app_runner.repository_url}:latest"
       image_repository_type = "ECR"
